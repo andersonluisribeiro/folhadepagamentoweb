189bfe15162f99b46ff39981d7b2b29bace16b39
Anderson Luis Ribeiro <anderson.luis.rib@gmail.com>
2016-03-19 09:25:13 -0300

    removendo codigo de teste

--- src/br/edu/unifeob/app/models/Programa.java
+++ src/br/edu/unifeob/app/models/Programa.java
@@ -4,47 +4,6 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Cargo cargo = new Cargo();
-		cargo.setDescricao("Analista de Sistemas Jr.");
-		cargo.setSalarioBase(5000.0);
-		cargo.setNivelDeInsalubridade(0.10);
-		
-		Empregado empregado = new Empregado();
-		empregado.setMatricula("1222");
-		empregado.setNome("Anderson Luis Ribeiro");
-		empregado.setCpf("111.111.111-11");
-		empregado.setCargo(cargo);
-		empregado.setQuantidadeDeDependentes(0);
-		
-		TabelaDeInss tabelaDeInss = new TabelaDeInss();
-		tabelaDeInss.setAnoDeVigencia(2015);
-		
-		TabelaDeIRRF tabelaDeIRRF = new TabelaDeIRRF();
-		tabelaDeIRRF.setAnoDeVigencia(2015);
-		tabelaDeIRRF.setValorPorDependente(189.59);
-		
-		SalarioMinimo salarioMinimo = new SalarioMinimo(1l, 2015, 788.0);
-		
-		FolhaDePagamento folha = new FolhaDePagamento();
-		folha.setMes(8);
-		folha.setAno(2015);
-		folha.setEmpregado(empregado);
-		folha.setTabelaDeInss(tabelaDeInss);
-		folha.setTabelaDeIRRF(tabelaDeIRRF);
-		folha.setSalarioMinimo(salarioMinimo);
-		folha.setAdiantamento(1000.00);
-		
-		System.out.println("O salário base da folha é:" + folha.salarioBase());
-		System.out.println("A insalubridade é de: " + folha.insalubridade());
-		System.out.println("O salário bruto da folha é: " + folha.salarioBruto());		
-		System.out.println("O inss é: " + folha.inss());
-		System.out.println("O valor do IRRF é: " + folha.impostoDeRenda());
-		System.out.println("O adiantamento é: " + folha.getAdiantamento());		
-		System.out.println("O salário líquido é: " + folha.salarioLiquido());
-		System.out.println("A base de cálculo é: " + folha.baseDeCalculoDoIRRF());
-		
-		
-		
 	}
 
 }
